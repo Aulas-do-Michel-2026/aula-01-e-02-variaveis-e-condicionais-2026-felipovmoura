3ee6b17c9895bd1bbb0100f423f02a10beaf0aa0
felipovmoura <felipovmoura@aol.com>
2026-05-02 20:16:27 -0300

    Update exercicio_5.py

--- exercicio_5.py
+++ exercicio_5.py
@@ -55,3 +55,21 @@ Resposta:
 Não
 
 """
+
+cromossomo = input("Digite o cromossomo: ")
+posicao = int(input("Digite a posição: "))
+genoma = input("Digite o genoma de referência: ")
+
+if cromossomo == "chr17":
+    
+    if genoma == "hg19" and 41196312 <= posicao <= 41277500:
+        print("Sim.")
+    
+    elif genoma == "hg38" and 43044295 <= posicao <= 43125483:
+        print("Sim.")
+    
+    else:
+        print("Não.")
+
+else:
+    print("Não.")
